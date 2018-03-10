949ff644e40d6807776d73194b0c4f0b1b27b884
Kyle Chambers <kylejchambers@gmail.com>
2018-03-10 10:31:00 -0800

    own component rendering with docker

--- server.js
+++ server.js
@@ -1,12 +1,28 @@
+
 const express = require('express')
 const morgan = require('morgan');
 const path = require('path');
+const axios = require('axios');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(morgan('dev'));
+//app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/listings/:id', express.static(path.join(__dirname, 'public')));
+app.use('/:id', express.static(path.join(__dirname, 'public')));
+
+app.use('/q-and-a', (req, res) => {
+  axios.get(`http://127.0.0.1:3004${req.originalUrl}`)
+    .then(moduleRes => moduleRes.data)
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      //console.log(err);
+      res.send();
+    });
+});
+
+
 
 
 app.listen(port, () => {
